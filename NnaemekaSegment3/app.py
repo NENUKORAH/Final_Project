3ca55d316d3fe9a228947101e4b6d2266754ea2e
NENUKORAH <81701640+NENUKORAH@users.noreply.github.com>
2021-09-26 18:18:24 -0400

    Update app.py

--- NnaemekaSegment3/app.py
+++ NnaemekaSegment3/app.py
@@ -1,14 +1,15 @@
+import os
 import dash
 import pandas as pd
 from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output
 import plotly.express as px
-import dash_bootstrap_components as bdc
 
 df = pd.read_csv("Tesla.csv")
 
 app = dash.Dash(__name__)
+server = app.server
 
 app.layout = html.Div([
 
@@ -18,7 +19,7 @@ app.layout = html.Div([
         id="ticker",
         options=[{"label": x, "value": x} 
                  for x in df.columns[1:]],
-        value=df.columns[1],
+        value=df.columns[8],
         clearable=False,
     ),
     dcc.Graph(id="time-series-chart"),
@@ -29,6 +30,9 @@ app.layout = html.Div([
     [Input("ticker", "value")])
 def display_time_series(ticker):
     fig = px.line(df, x='date', y=ticker, title='Tesla Financial Information Trend')
+    fig.update_xaxes(rangeslider_visible=True)
     return fig
 
-app.run_server(debug=True)
+if __name__ == '__main__':
+    app.run_server(debug=True)
+    
